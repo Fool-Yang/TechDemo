98504e6f2028bee6108a4effbe9a28972ae06575
Yang <39217646+Fool-Yang@users.noreply.github.com>
2022-04-25 15:31:40 +0100

    Initial commit

---
+++ Assets/Agent/PlayerUnit.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace UnityMovementAI
+{
+    public class WanderAvoidUnit : MonoBehaviour
+    {
+        SteeringBasics steeringBasics;
+
+        void Start()
+        {
+            steeringBasics = GetComponent<SteeringBasics>();
+        }
+
+    }
+}
